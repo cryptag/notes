b8c102a52c5019f3485e314eeedc1ad0b32514ac
Steve Phillips <steve@tryingtobeawesome.com>
2017-01-16 19:19:11 -0800

    Removed unnecessary 'Title' label + fixed styling

--- app/components/wiki/WikiPageEdit.js
+++ app/components/wiki/WikiPageEdit.js
@@ -86,7 +86,6 @@ class WikiPageEdit extends Component {
             <button className="btn btn-default" onClick={this.onCancelClick}>Cancel</button>
           </div>
           <div className="form-group page-title-bar" ref="page_title">
-            <label>Title</label>
             <input className="form-control" value={title} placeholder="Enter page title" readOnly={readOnly} onChange={this.onUpdateTitle}/>
           </div>
           <Nav bsStyle="tabs" activeKey={activeKey} onSelect={this.onChangeEditMode}>
