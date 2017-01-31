7dc4b633640a54f8e117c57985f751e2a09dfa17
Steve Phillips <steve@tryingtobeawesome.com>
2017-01-31 13:21:06 -0800

    Error handling: data/api/handleResponse now always returns response as JSON rather than an Error when !response.ok

--- app/data/api.js
+++ app/data/api.js
@@ -41,7 +41,7 @@ const requestGet = function(urlSuffix, backendName){
 
 let handleResponse = function(response) {
   if (!response.ok) {
-    return new Error(response.statusText);
+    console.log("Error response:", response);
   }
   return response.json()
 }
